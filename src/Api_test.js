9b9b2d5af4e3a3eb58c15c499b7b857111535902
Ruslan <ruslanbek05@gmail.com>
2021-10-06 10:37:39 +0500

    hello world

---
+++ src/Api_test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ApiTest = () => {
+    return (
+        <div>
+            hello world
+        </div>
+    );
+};
+
+export default ApiTest;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Api_test from "./Api_test";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        {/*<App />*/}
+        <Api_test/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
