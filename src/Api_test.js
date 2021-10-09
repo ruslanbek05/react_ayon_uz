84c3435aca950bce4806597466ca61e888491b33
Ruslan <ruslanbek05@gmail.com>
2021-10-10 01:09:47 +0500

    Update Api_test.js

--- src/Api_test.js
+++ src/Api_test.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { Component } from "react";
+import axios from "axios";
+import './App.css';
+class Api_test extends Component{
 
-const ApiTest = () => {
-    return (
-        <div>
-            hello world
-        </div>
-    );
-};
+    constructor() {
+        super();
+        this.state = {
+            url: '',
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
 
-export default ApiTest;
+    handleChange(event) {
+        this.setState({url: event.target.value})
+    }
+
+    handleSubmit(event) {
+        axios
+            // .post("/url_checker", this.state.url)
+            .post("http://127.0.0.1:8000/url_checker/", this.state.url)
+            .then(res => {
+                alert(res.data)
+            })
+        event.preventDefault();
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <form onSubmit={this.handleSubmit}>
+                        <label>
+                            url:
+                            <input type="text" name="url" value={this.state.url} onChange={this.handleChange} />
+                        </label>
+                        <input type="submit" value="Check URL" />
+                    </form>
+                </header>
+            </div>
+        );
+    }
+
+}
+export default Api_test;
