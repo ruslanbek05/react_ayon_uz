47c6cdabe80dceec379f47324441e75df2bf0a9e
Ruslan <ruslanbek05@gmail.com>
2021-10-25 00:20:35 +0500

    Viewsets and Routers with React Front-end Example - Part-4

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import App from './App';
 import Header from './components/header';
 import Footer from './components/footer';
-import Register from './components/registeer';
+import Register from './components/register';
 import Login from './components/login';
 import Logout from './components/logout';
+import Single from './components/single';
 
 const routing = (
     <Router>
@@ -18,6 +19,7 @@ const routing = (
                 <Route exact path="/register" component={Register} />
                 <Route exact path="/login" component={Login} />
                 <Route exact path="/logout" component={Logout} />
+                <Route path="/post/:slug" component={Single} />
             </Switch>
             <Footer />
         </React.StrictMode>
